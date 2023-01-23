1015b6f4089eccf954a7009f63fb871f03844675
infinitus88 <79045912+infinitus88@users.noreply.github.com>
2023-01-24 00:41:24 +0500

    Update default password for admin

--- src/ArmedMFG.ApplicationCore/Constants/AuthorizationConstants.cs
+++ src/ArmedMFG.ApplicationCore/Constants/AuthorizationConstants.cs
@@ -5,7 +5,7 @@ public class AuthorizationConstants
     public const string AUTH_KEY = "AuthKeyOfDoomThatMustBeAMinimumNumberOfBytes";
 
     // TODO: Don't use this in production
-    public const string DEFAULT_PASSWORD = "228265666";
+    public const string DEFAULT_PASSWORD = "Rustam022001@";
 
     // TODO: Change this to an environment variable
     public const string JWT_SECRET_KEY = "SecretKeyOfDoomThatMustBeAMinimumNumberOfBytes";
